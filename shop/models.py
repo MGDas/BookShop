b7c77b555280b06003d4de13469c703a31c213ce
MGDas <mgdas051@gmail.com>
2019-12-27 14:00:24 +0300

    Create Author model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=250, db_index=True)
+    biography = models.TextField(blank=True)
+    birthday = models.DateField(blank=True)
+    death = models.DateField(blank=True)
+
+    def __str__(self):
+        return self.name
